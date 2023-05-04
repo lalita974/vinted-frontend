054ff2a9633bf72f82effff4f904ff8dc12564a3
Lalita <lalita.alibay@hotmail.fr>
2023-05-04 14:23:22 +0000

    ajout page offer

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,6 @@ const App = () => {
     <Router>
       {/* Le bloc ci dessous va apparaitre sur toutes mes pages */}
       <Header />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/offer:id">Offers</Link>
-          </li>
-        </ul>
-      </nav>
       {/* Le composant Routes doit contenir toutes mes Route il affiche un composant à la fois */}
       <Routes>
         {/* Pour chaque route, je précise son chemin et le composant qu'elle doit afficher */}
--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Offer = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams();
-  return (
-    <div>
-      <h2>Product Component</h2>
-      <span>The Offer id is : {id}</span>
-    </div>
-  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://lereacteur-vinted-api.herokuapp.com/offer/${id}`
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  return isLoading ? <p>Chargement en cours</p> : <div>{data}</div>;
 };
 
 export default Offer;
